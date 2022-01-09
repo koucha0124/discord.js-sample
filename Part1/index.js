c5cd8de84556b602ee75c90ca207b1389e21dc69
Koucha0124 <84267040+Koucha0124@users.noreply.github.com>
2022-01-09 20:26:56 +0900

    Update index.js

--- Part1/index.js
+++ Part1/index.js
@@ -1,14 +1,14 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async msg => {
-  if (msg.content === '!ping') {
-    msg.channel.send('Pong!');
-  }
-})
+client.on ('messageCreate', (message) => {
+    if (message.content === 'hello') {
+        message.reply('Hello')
+    };
+});
 
 client.login("token");
