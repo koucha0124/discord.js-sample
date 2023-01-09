b49fcc157f7296dd5abe85f38b8515f249bbd423
こうちゃ <84267040+koucha0124@users.noreply.github.com>
2023-01-09 23:45:24 +0900

    Update index.js

--- Part1/index.js
+++ Part1/index.js
@@ -1,5 +1,5 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
